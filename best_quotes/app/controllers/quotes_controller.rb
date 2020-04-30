9e475b98bea787d31c1204e3f92e030bdda2f81a
wildeng <alain.mauri@gmail.com>
2020-04-30 07:53:47 +0100

    Added code for chapter 4 excercises

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,10 +1,19 @@
 # best_quotes/app/controllers/quotes_controller.rb
 class QuotesController < Rulers::Controller
+  
+	attr_accessor :initial_quote
+  attr_accessor :final_quote
+
   def a_quote
+		@initial_quote = "Initial quote"
+		@final_quote = "Final quote"
     render :a_quote, noun: :winking
   end
   
   def a_change
+		@initial_quote = "Initial change"
+		@final_quote = "Final change"
+
     render :a_change
   end
   

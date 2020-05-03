b9e426988ee92fc24060e6b5f72cba5f1d7e5382
wildeng <alain.mauri@gmail.com>
2020-05-03 17:44:56 +0100

    added update method to controller

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,15 +1,43 @@
 # best_quotes/app/controllers/quotes_controller.rb
 class QuotesController < Rulers::Controller
-  
+
 	attr_accessor :initial_quote
   attr_accessor :final_quote
+
+  def index
+    quotes = FileModel.all
+    render :index, quotes: quotes
+  end
+
+  def update
+    # TODO: this needs to be more general
+    file_model = FileModel.find(1)
+    attrs = {
+      'submitter' => 'Super Mario',
+      'quote' => file_model['quote'],
+      'attribution' => file_model['attribution']
+    }
+    file_model.update_attrs(attrs)
+    render :quote, obj: file_model
+  end
+
+  def new_quote
+    attr = {
+      'submitter' => 'web user',
+      'quote' => 'A picture is worth k pixels',
+      'attribution' => 'Noah Gibbs'
+    }
+
+    m = FileModel.create attr
+    render :quote, obj: m
+  end
 
   def a_quote
 		@initial_quote = "Initial quote"
 		@final_quote = "Final quote"
     render :a_quote, noun: :winking
   end
-  
+
   def a_change
 		@initial_quote = "Initial change"
 		@final_quote = "Final change"
@@ -18,10 +46,10 @@ class QuotesController < Rulers::Controller
   end
 
   def quote_1
-    quote_1 = Rulers::Model::FileModel.find(1)
+    quote_1 = FileModel.find(1)
     render :quote, obj: quote_1
   end
-  
+
   def exception
     raise "It's a bad one!"
   end

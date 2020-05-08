e7e1b3d6c2809676313ec530ae5090b9d6cf56d1
wildeng <alain.mauri@gmail.com>
2020-05-08 13:28:58 +0100

    Added a first example of cache

--- rulers/lib/rulers/file_model.rb
+++ rulers/lib/rulers/file_model.rb
@@ -3,16 +3,19 @@ require 'multi_json'
 module Rulers
   module Model
     class FileModel
+      attr_accessor :caching
+      attr_accessor :file_cache
       @@db_folder = 'db/quotes'
 
       def initialize(filename)
+        @caching = true
+        @file_cache = []
         @filename = filename
-
-        # if filename is "dir/37.json", @id is 37
         basename = File.split(filename)[-1]
         @id = File.basename(basename, ".json").to_i
-
-        obj = File.read(filename)
+        obj = obj_from_cache
+        obj = File.read(filename) unless obj
+        push_obj_to_cache(obj)
         @hash = MultiJson.load(obj)
       end
       
@@ -103,11 +106,43 @@ module Rulers
         File.open("#{@@db_folder}/#{id}.json", "w") do |f|
           f.write(template) 
         end
-
+        if obj_in_cache?
+          remove_obj_from_cache
+        end
         FileModel.new "#{@@db_folder}/#{id}.json"
       end
 
       private
+
+      def obj_from_cache
+        return unless caching?
+        puts "Loading object with id: #{@id} from cache"
+        @file_cache.select { |obj| obj[:id] == @id }.first 
+      end
+
+      def remove_obj_from_cache
+        puts "Removing object with id: #{@id} from cache"
+        @file_cache.delete { |obj| obj[:id] == @id }
+      end
+
+      def push_obj_to_cache(obj)
+        if caching?
+          puts "Saving object with id: #{@id} to cache"
+          @file_cache.push(
+            { id: @id, obj: obj}
+          ) unless obj_in_cache?
+          @file_cache.sort_by! { |obj| obj[:id]}
+        end
+      end
+
+      def obj_in_cache?
+        puts "Object with id: #{@id} is already saved in cache"
+        @file_cache.any? { |obj| obj[:id] == @id }
+      end
+
+      def caching?
+        @caching
+      end
 
       def update(attrs)
         self['submitter'] = attrs['submitter']
